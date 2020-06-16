fd368afddfd10659c075b6878ddbf29e27685e31
reddevilcero <reddevil_cero@hotmail.com>
2020-06-16 13:41:10 +0100

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class CreateTodo extends Component {
+  state = {
+    todo: "",
+  };
+
+  handleOnChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodo(this.state);
+    this.setState({ todo: "" });
+  };
+
   render() {
-    return(
+    {
+      console.log(this.props);
+    }
+    return (
       <div>
-        Create Todo Component
+        <form onSubmit={this.handleOnSubmit}>
+          <p>
+            <label htmlFor="todo">Add Todo</label>
+            <input
+              type="text"
+              name="todo"
+              onChange={this.handleOnChange}
+              value={this.state.todo}
+            />
+          </p>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default CreateTodo;
+const mapStateToProps = (state) => ({
+  todos: state.todos
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (todo) => dispatch({ type: "ADD_TODO", payload: todo }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateTodo);
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,18 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
-
+export default function manageTodo(
+  state = {
+    todos: [],
+  },
+  action
+) {
   console.log("reducer received this action:", action);
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
 
-  return state;
+    default:
+      return state;
+  }
 }
